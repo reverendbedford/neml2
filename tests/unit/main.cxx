700ce192d29f5f3e1293873c8b2917eba7e2b304
Gary Hu <hugary1995@gmail.com>
2023-07-29 18:26:36 -0500

    Disable unity build for catch2 tests

--- tests/unit/main.cxx
+++ tests/unit/main.cxx
@@ -22,34 +22,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#define CATCH_CONFIG_MAIN
+
 #include <catch2/catch.hpp>
-
-#include "neml2/tensors/BatchTensor.h"
-
-using namespace neml2;
-
-TEST_CASE("BatchTensors have the correct shapes", "[BatchTensors]")
-{
-  // Should have 2 batch dimensions and 3 base dimensions
-  BatchTensor<2> A(torch::zeros({10, 5, 4, 11, 2}));
-
-  SECTION(" batch sizes are correct")
-  {
-    REQUIRE(A.batch_dim() == 2);
-    REQUIRE(A.batch_sizes() == TorchShape({10, 5}));
-  }
-
-  SECTION(" base sizes are correct")
-  {
-    REQUIRE(A.base_dim() == 3);
-    REQUIRE(A.base_sizes() == TorchShape({4, 11, 2}));
-  }
-}
-
-TEST_CASE("BatchTensors can't be created with few than the number of "
-          "batch dimensions")
-{
-#ifndef NDEBUG
-  REQUIRE_THROWS(BatchTensor<2>(torch::zeros({10})));
-#endif
-}
