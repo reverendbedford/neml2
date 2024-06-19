42bf61c5129a930753a7c3b46680de3f1c4dadb4
Gary Hu <thu@anl.gov>
2024-06-19 17:00:53 -0500

    FB complementarity condition

--- src/neml2/models/solid_mechanics/RateIndependentPlasticFlowConstraint.cxx
+++ src/neml2/models/solid_mechanics/RateIndependentPlasticFlowConstraint.cxx
@@ -66,32 +66,44 @@ RateIndependentPlasticFlowConstraint::set_value(bool out, bool dout_din, bool d2
 {
   neml_assert_dbg(!d2out_din2, "Second derivative not implemented.");
 
-  // The residual is the yield function itself when the stress state is "outside" the yield surface,
-  // also called return mapping. The residual is the hardening rate when the stress state is
-  // "inside" the yield surface, as the hardening rate is by definition zero.
-  const auto yielding = _fp >= -_ytol;
-  const auto not_yielding = torch::logical_not(yielding);
+  const auto FB = _gamma_dot - _fp - math::sqrt(_gamma_dot * _gamma_dot + _fp * _fp);
 
   if (out)
-  {
-    auto r = Scalar::empty_like(_r);
-    r.index_put_({not_yielding}, Scalar(_gamma_dot).index({not_yielding}));
-    r.index_put_({yielding}, Scalar(_fp).index({yielding}));
-    _r = r;
-  }
+    _r = FB;
 
   if (dout_din)
   {
-    auto dr_dgamma_dot = Scalar::empty_like(_r);
-    dr_dgamma_dot.index_put_({not_yielding}, 1);
-    dr_dgamma_dot.index_put_({yielding}, 0);
-    _r.d(_gamma_dot) = dr_dgamma_dot;
-
-    auto dr_dfp = Scalar::empty_like(_r);
-    dr_dfp.index_put_({not_yielding}, 0);
-    dr_dfp.index_put_({yielding}, 1);
-    _r.d(_fp) = dr_dfp;
+    const auto I = Scalar::identity_map(options());
+    _r.d(_gamma_dot) = I - _gamma_dot / math::sqrt(_gamma_dot * _gamma_dot + _fp * _fp);
+    _r.d(_fp) = -I - _fp / math::sqrt(_gamma_dot * _gamma_dot + _fp * _fp);
   }
+
+  // The residual is the yield function itself when the stress state is "outside" the yield surface,
+  // also called return mapping. The residual is the hardening rate when the stress state is
+  // "inside" the yield surface, as the hardening rate is by definition zero.
+  // const auto yielding = _fp >= -_ytol;
+  // const auto not_yielding = torch::logical_not(yielding);
+
+  // if (out)
+  // {
+  //   auto r = Scalar::empty_like(_r);
+  //   r.index_put_({not_yielding}, Scalar(_gamma_dot).index({not_yielding}));
+  //   r.index_put_({yielding}, Scalar(_fp).index({yielding}));
+  //   _r = r;
+  // }
+
+  // if (dout_din)
+  // {
+  //   auto dr_dgamma_dot = Scalar::empty_like(_r);
+  //   dr_dgamma_dot.index_put_({not_yielding}, 1);
+  //   dr_dgamma_dot.index_put_({yielding}, 0);
+  //   _r.d(_gamma_dot) = dr_dgamma_dot;
+
+  //   auto dr_dfp = Scalar::empty_like(_r);
+  //   dr_dfp.index_put_({not_yielding}, 0);
+  //   dr_dfp.index_put_({yielding}, 1);
+  //   _r.d(_fp) = dr_dfp;
+  // }
 }
 
 } // namespace neml2
