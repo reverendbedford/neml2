37c9052153cbdb94af1cf36287238c323381feb9
Gary (Tianchen) Hu <thu@anl.gov>
2024-06-20 23:08:55 -0500

    Update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -32,7 +32,7 @@ export COVERAGE_DIR=$ROOT/coverage
 mkdir -p $COVERAGE_DIR
 lcov --gcov-tool gcov --capture --initial --directory $SRC_DIR --output-file $COVERAGE_DIR/initialize.info
 cd tests
-../build/tests/unit/unit_tests || true
+./unit/unit_tests || true
 lcov --gcov-tool gcov --capture --ignore-errors gcov,source --directory $SRC_DIR --output-file $COVERAGE_DIR/covered.info
 lcov --gcov-tool gcov --add-tracefile $COVERAGE_DIR/initialize.info --add-tracefile $COVERAGE_DIR/covered.info --output-file $COVERAGE_DIR/final.info
 lcov --gcov-tool gcov --extract $COVERAGE_DIR/final.info \*$SRC_DIR/\* --extract $COVERAGE_DIR/final.info \*$INCLUDE_DIR/\* --output-file $COVERAGE_DIR/coverage.info
