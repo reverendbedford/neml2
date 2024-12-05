53b703529b7738c53b2ca28a241558a38f5cf912
Daniel Schwen <daniel@schwen.de>
2024-12-05 13:42:09 -0700

    Scalar trial stress update (#173)
    * Scalar trial stress update (#173)
    * Modify the input file slightly so that the gold file can be reused
    * Add unit tests for the two new objects
    
    ---------
    
    Co-authored-by: Gary Hu <thu@anl.gov>

---
+++ include/neml2/models/solid_mechanics/AssociativeJ2FlowDirection.h
@@ -0,0 +1,48 @@
+// Copyright 2024, UChicago Argonne, LLC
+// All Rights Reserved
+// Software Name: NEML2 -- the New Engineering material Model Library, version 2
+// By: Argonne National Laboratory
+// OPEN SOURCE LICENSE (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#pragma once
+
+#include "neml2/models/Model.h"
+
+namespace neml2
+{
+/// The plastic flow direction assuming an associative J2 flow.
+class AssociativeJ2FlowDirection : public Model
+{
+public:
+  static OptionSet expected_options();
+
+  AssociativeJ2FlowDirection(const OptionSet & options);
+
+protected:
+  virtual void set_value(bool, bool, bool) override;
+
+  /// Mandel stress
+  const Variable<SR2> & _M;
+
+  /// Flow direction
+  Variable<SR2> & _N;
+};
+} // namespace neml2
