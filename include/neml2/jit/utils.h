9a65bbd06ed56695d5d955440215c2a53f27c471
Gary Hu <thu@anl.gov>
2024-12-07 19:37:03 -0600

    Fix unity build

--- include/neml2/jit/utils.h
+++ include/neml2/jit/utils.h
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#pragma once
+
 #include <torch/jit.h>
 #include <torch/csrc/jit/frontend/tracer.h>
 
